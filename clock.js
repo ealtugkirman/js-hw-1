0c9af1e655704670226b1288af89ab5f01020e8a
ealtugkirman <110311214+ealtugkirman@users.noreply.github.com>
2022-08-22 15:24:01 +0300

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,23 @@
+let days = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];
+let Name = prompt("adınız")
+let myName = document.querySelector("#myName");
+
+if(Name) {
+    myName.innerHTML = `${Name} `
+}
+    else{
+    alert("Bir isim giriniz")
+    location.reload();
+}   
+
+
+function clockFunction(){
+    let dateTime= new Date();
+    let day = days[dateTime.getDay()];
+    let hrs = dateTime.getHours();
+    let min = dateTime.getMinutes();
+    let sec = dateTime.getSeconds();
+    document.querySelector("#myClock").innerHTML= `${hrs}: ${min}: ${sec}  ${day}`
+}
+    
+    setInterval(clockFunction, 1000);
